1784e6cb65a073658a59d3855c9d5d2645e6438e
Viktor Martyniuk <viktor.mart@gmail.com>
2017-06-05 13:45:55 +0300

    css/less handling

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,8 @@
 import React     from 'react';
 import PropTypes from 'prop-types';
 
+import './UserCard.less';
+
 export default class UserCard extends React.Component {
   static propTypes = {
     userName: PropTypes.string,
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-const path    = require("path");
-const webpack = require("webpack");
+const path              = require("path");
+const webpack           = require("webpack");
+const autoprefixer      = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const env = process.env.NODE_ENV;
 
@@ -13,13 +15,38 @@ const config = {
         filename: "bundle.js"
     },
     module: {
-        loaders: [{
+        rules: [
+          {
+              test: /\.less$/,
+              loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                  { loader: 'css-loader', options: { importLoaders: 1 } },
+                  { loader: 'postcss-loader', options: { plugins: [ autoprefixer ]}},
+                  'less-loader'
+                ]
+              })
+          },
+          {
+              test: /\.css$/,
+              loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                  { loader: 'css-loader', options: { importLoaders: 1 } },
+                  { loader: 'postcss-loader', options: { plugins: [ autoprefixer ]}}
+                ]
+              })
+          },
+          {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
-        }]
+          }
+        ]
     },
-    plugins: []
+    plugins: [
+      new ExtractTextPlugin({ filename: 'style.css', allChunks: true })
+    ]
 };
 
 if (env === 'production') {
