9e649f2ca3c1cfdb4ef5906b7bf8a3ef9b659f35
Viktor Martyniuk <viktor.mart@gmail.com>
2019-03-27 16:09:59 +0200

    update dependencies: webpack, loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,66 @@
-const path              = require("path");
-const webpack           = require("webpack");
-const autoprefixer      = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const env = process.env.NODE_ENV;
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-    entry: {
-        app: [path.resolve(__dirname, 'src', 'App.jsx')]
-    },
+    entry: path.resolve(__dirname, 'src', 'App.jsx'),
     output: {
-        path: path.resolve(__dirname, 'public', 'static_assets'),
-        publicPath: "/static_assets/",
-        filename: "bundle.js"
+        filename: 'main.js',
+        path: path.join(__dirname, (isProduction ? 'build' : 'public'))
     },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+
+    devtool: isProduction ? '' : 'source-map',
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+
     module: {
         rules: [
-          {
-              test: /\.less$/,
-              loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
+            {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader'
+            },
+
+            {
+                test: /\.less$/,
                 use: [
-                  { loader: 'css-loader', options: { importLoaders: 1 } },
-                  { loader: 'postcss-loader', options: { plugins: [ autoprefixer ]}},
-                  'less-loader'
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [ autoprefixer ]
+                        }
+                    },
+                    'less-loader'
                 ]
-              })
-          },
-          {
-              test: /\.css$/,
-              loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [
-                  { loader: 'css-loader', options: { importLoaders: 1 } },
-                  { loader: 'postcss-loader', options: { plugins: [ autoprefixer ]}}
-                ]
-              })
-          },
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-          }
+            }
         ]
     },
+
     plugins: [
-      new ExtractTextPlugin({ filename: 'style.css', allChunks: true })
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'templates', 'index.html')
+        })
     ]
 };
 
-if (env === 'production') {
-    config.plugins.push(new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify('production')
-        }
+if (isProduction) {
+    config.plugins.push(new MiniCssExtractPlugin({
+        filename: '[name].css'
     }));
 
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({}));
+    config.plugins.push(new CleanWebpackPlugin());
 }
 
 module.exports = config;
